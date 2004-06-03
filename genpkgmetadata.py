9aea9699d9e4dd58d470967f256754f9b2ce0a8b
Seth Vidal <skvidal@linux.duke.edu>
2004-06-03 06:14:57 +0000

    fix stupid version thing

--- genpkgmetadata.py
+++ genpkgmetadata.py
@@ -33,7 +33,7 @@ from zlib import error as zlibError
 from gzip import write32u, FNAME
 
 import dumpMetadata
-__version__ = '0.1'
+__version__ = '0.3.2'
 
 def errorprint(stuff):
     print >> sys.stderr, stuff
